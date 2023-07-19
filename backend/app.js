4684d54087852030eac6d823a8cd76e09e5e7e8b
Shivam <shivam12061999@gmail.com>
2023-07-20 04:37:07 +0530

    First Commit

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+//Routes Import
+const product = require('./routes/productRoute');
+
+app.use("/api/v1",product);
+
 module.exports = app;
---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = async () => {
+    try {
+      await mongoose.connect(process.env.DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+  
+      // Define your models and their schemas here...
+  
+      console.log(`Mongodb connected with server ${mongoose.connection.host}`);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+module.exports = connectDatabase;
