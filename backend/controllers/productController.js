ed4b7ea1d0132150aa6c214702294e4a488e9b46
Shivam <shivam12061999@gmail.com>
2023-07-23 01:50:43 +0530

    added create functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,34 @@
-const product = require("../models/productModel");
+const Product = require("../models/productModel");
 
 
-//Create Product
+//Create Product --ADMIN
+exports.createProduct = async(req,res,nextf)=>{
+    const product = await Product.create(req.body);
 
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
 
-exports.getAllProducts = (req,res) => {
-    res.status(200).json({message:"Route is working"});
+//Get All Products --PUBLIC
+exports.getAllProducts = async(req,res) => {
+    const products = await Product.find();
+    
+    res.status(200).json({
+        success:true,
+        products
+    })
+}
+
+//Update Product --ADMIN
+exports.updateProduct = async(req,res,next) => {
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
 }
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,6 @@ dotenv.config({path:"backend/config/config.env"});
 //Connecting to database
 connectDatabase();
 
-// app.listen(process.env.PORT, () => {
-//     console.log(`Server is working on http://localhost:${process.env.PORT}`);
-// })
+app.listen(process.env.PORT, () => {
+    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+})
